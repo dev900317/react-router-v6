b9d031bfda64ff123d500d694486ac6a34b4096b
dev900317 <bartosz.zagorski@icloud.com>
2022-02-10 23:11:52 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import { Link, Outlet } from 'react-router-dom';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bookeeper</h1>
+      <nav>
+        <Link to="/invoices">Invoices</Link> |{' '}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
     </div>
   );
-}
+};
 
 export default App;
